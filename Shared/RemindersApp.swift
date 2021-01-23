a0e4f53ea7519811ab5588710995bfa9184e13bd
James Wu <ywu@lcs.on.ca>
2021-01-22 23:45:31 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by James Wu on 2021-01-22.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
